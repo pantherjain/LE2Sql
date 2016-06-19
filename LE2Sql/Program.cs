133fde59436a66f905b1ce8545c86e6b9f90ecb8
WalterMitty <WalterMitty@WALTERMITTY-PC>
2016-06-19 16:04:10 +0800

    Add project files.

---
+++ LE2Sql/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LE2Sql
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+
+			var repo = new Repository<Product>();
+
+			// 單筆更新
+			var customer = new Customer { AddQty = 5 };
+			repo.Update(
+			SET: p => new Product
+			{
+				Qty = p.Qty + customer.AddQty
+			},
+			WHERE: p => p.ProductName == "123" && p.Qty > customer.AddQty);
+
+
+			// 多筆更新
+			var customers = new List<Customer>();
+			repo.Update(
+			SET: (p, c) => new Product
+			{
+				Qty = p.Qty + c.AddQty
+			},
+			WHERE: (p, c) => p.ProductName == "123" && p.Qty > c.AddQty,
+			parms: customers);
+
+			// 額外固定比對欄位
+			repo.Update(
+			SET: (p, c, f) => new Product
+			{
+				Qty = p.Qty + c.AddQty,
+				AccountOpened = f.OwnerName
+			},
+			WHERE: (p, c, f) => p.ProductName == f.Name && p.Qty > c.AddQty,
+			parms: customers,
+			fixedParam: new { OwnerName = "Boss", Name = "123" });
+
+		}
+
+	
+		public class Product
+		{
+			public decimal ProductID { get; set; }
+			public string ProductName { get; set; }
+			public string AccountOpened { get; set; }
+			public Customer Customer { get; set; }
+			public int Qty { get; set; }
+		}
+
+		public class Customer
+		{
+			public decimal CustomerId { get; set; }
+			public string CustomerName { get; set; }
+			public int AddQty { get; set; }
+		}
+
+
+	}
+
+
+}
---
+++ LE2Sql/Repository.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LE2Sql
+{
+	public class Repository<T>
+		where T : class
+	{
+
+		public int Update(
+			Expression<Func<T, T>> SET,
+			Expression<Func<T, bool>> WHERE = null)
+		{
+			throw new NotImplementedException();
+		}
+
+		public int Update<TParam>(
+			Expression<Func<T, TParam, T>> SET,
+			Expression<Func<T, TParam, bool>> WHERE,
+			IEnumerable<TParam> parms)
+		{
+			throw new NotImplementedException();
+		}
+
+		public int Update<TParam1, TParam2>(
+			Expression<Func<T, TParam1, TParam2, T>> SET,
+			Expression<Func<T, TParam1, TParam2, bool>> WHERE,
+			IEnumerable<TParam1> parms, TParam2 fixedParam)
+		{
+			throw new NotImplementedException();
+		}
+
+	}
+}
